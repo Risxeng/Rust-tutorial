e1d3bb4760e2b1294f83ab1db28e9e7515d70c6b
Rishav <rishavofficial0709@gmail.com>
2025-10-03 07:14:47 +0000

    .

--- viteprojectapp/src/main.jsx
+++ viteprojectapp/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// import "./index.css"; // make sure this file exists
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Create root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render App
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
+
