b35fc1ef29f73bed7a0dec86a8c58db00ca2ac05
Rishav <rishavofficial0709@gmail.com>
2025-10-10 06:44:52 +0000

    .

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,11 +4,48 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter, setCounter] = useState(13)
+  // let counter = 15
+                                          //HW
+  const addvalue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1)
+      console.log("value added" , counter+1);
+    }else{
+      console.log("value excedded");
+    }
+  }
+  const removevalue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+      console.log("value added" , counter-1);
+    }else{
+      console.log("counter can't be negative");
+    }
+  }
+
+                                          //practise
+  // const addvalue = () => {
+  //   console.log("value added", counter);
+  //   counter = counter + 1
+  //   setCounter(counter) 
+  // }
+  // const removevalue = () => {
+  //   console.log("value removed ", counter);
+  //   counter = counter - 1;
+  //   setCounter(counter)
+  // }
 
   return (
     <>
       <h1>chai aur react</h1>
+      <h2>counter value: {counter}</h2>
+
+      <button
+      onClick={addvalue}>Add Value {counter}</button>
+      <br />
+      <button onClick={removevalue}>Remove Value {counter}</button>
+      <p>footer: {counter} </p>
     </>
   )
 }
